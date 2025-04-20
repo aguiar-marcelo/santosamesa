3ad1cf8e1c3b2ac649649efece5a3821af855251
bandeiraceci <cecilleolbandeira@gmail.com>
2025-04-19 22:39:24 -0300

    fix: tela de cadastro

--- src/components/RegisterPage.tsx
+++ src/components/RegisterPage.tsx
@@ -4,61 +4,120 @@ import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { OrbitProgress } from "react-loading-indicators";
+import './css/RegisterPage.css';
 
 const Cadastro = () => {
   const [etapa, setEtapa] = useState(1);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [generalError, setGeneralError] = useState<string | null>(null);
+  const [fullName, setFullName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+  const [profilePicturePreview, setProfilePicturePreview] = useState<string | null>(null);
+  const [profilePictureFile, setProfilePictureFile] = useState<File | null>(null);
+
+  const [userNameError, setUserNameError] = useState<string | null>(null);
+  const [emailError, setEmailError] = useState<string | null>(null);
+  const [passwordError, setPasswordError] = useState<string | null>(null);
+  const [confirmPasswordError, setConfirmPasswordError] = useState<string | null>(null);
+  const [fullNameError, setFullNameError] = useState<string | null>(null);
+  const [fileError, setFileError] = useState<string | null>(null);
+
+  const router = useRouter();
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files?.[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+      setProfilePictureFile(selectedFile);
+      setFileError(null);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setProfilePicturePreview(reader.result as string);
+      };
+      reader.readAsDataURL(selectedFile);
+    } else {
+      setFile(null);
+      setProfilePictureFile(null);
+      setProfilePicturePreview(null);
+      setFileError("Selecione uma imagem de perfil.");
+    }
+  };
+
+  const proximaEtapa = () => {
+    setGeneralError(null);
+    setUserNameError(null);
+    setEmailError(null);
+    setPasswordError(null);
+    setConfirmPasswordError(null);
+
+    let isValid = true;
+
+    const userNameRegex = /^[a-z0-9_-]+$/;
+    if (!userNameRegex.test(userName)) {
+      setUserNameError("Nome de usuário deve conter apenas letras minúsculas, números, hífen (-) e underline (_), sem espaços.");
+      isValid = false;
+    }
+
+    if (!email) {
+      setEmailError("'Endereço de e-mail' não pode ser vazio.");
+      isValid = false;
+    } else {
+      const emailRegex = /.+@/;
+      if (!emailRegex.test(email)) {
+        setEmailError("Por favor, insira um endereço de e-mail válido.");
+        isValid = false;
+      }
+    }
+
+    if (!password) {
+      setPasswordError("A senha não pode ser vazia.");
+      isValid = false;
+    } else if (password.length < 8) {
+      setPasswordError("A senha precisa ter no mínimo 8 caracteres.");
+      isValid = false;
+    }
+
+    if (!confirmPassword) {
+      setConfirmPasswordError("A confirmação da senha não pode ser vazia.");
+      isValid = false;
+    }
+
+    if (password !== confirmPassword) {
+      setConfirmPasswordError("As senhas não coincidem.");
+      isValid = false;
+    }
+
+    if (isValid) {
+      setEtapa(2);
+    }
+  };
 
   const etapaAnterior = () => {
     setEtapa(1);
   };
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-  const [fullName, setFullName] = useState("");
-  const [userName, setUserName] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [file, setFile] = useState<File | null>(null);
-
-  const router = useRouter();
-
   const register = async () => {
+    setGeneralError(null);
+    setFullNameError(null);
+    setFileError(null);
+
+    let isValid = true;
+
     if (!fullName.trim().includes(" ")) {
-      alert("Digite seu nome completo com sobrenome.");
-      return;
-    }
-
-    const userNameRegex = /^[a-z0-9_-]+$/;
-    if (!userNameRegex.test(userName)) {
-      alert(
-        "Username deve conter apenas letras minúsculas, números, hífen (-) e underline (_), sem espaços."
-      );
-      return;
-    }
-
-    if (!email) {
-      alert("Digite o email");
-      return;
-    }
-
-    if (!password || !confirmPassword) {
-      alert("Digite a senha e confirme.");
-      return;
-    }
-
-    if (password.length < 8) {
-      alert("A senha deve ter pelo menos 8 caracteres.");
-      return;
-    }
-
-    if (password !== confirmPassword) {
-      alert("As senhas não coincidem.");
-      return;
+      setFullNameError("Nome de exibição não pode ser vazio.");
+      isValid = false;
     }
 
     if (!file) {
-      alert("Selecione uma imagem de perfil.");
+      setFileError("Selecione uma imagem de perfil.");
+      isValid = false;
+    }
+
+    if (!isValid) {
       return;
     }
 
@@ -68,141 +127,166 @@ const Cadastro = () => {
     formData.append("role", "user");
     formData.append("userName", userName);
     formData.append("exibitionName", fullName);
-    formData.append("profilePicture", file);
+
+    if (file) {
+      formData.append("profilePicture", file);
+    }
 
     setLoading(true);
-    setError(null);
-  
+    setGeneralError(null);
+
     try {
       const response = await postRegister(formData);
       alert("Usuário cadastrado com sucesso!");
       router.push("/login");
     } catch (error: any) {
       if (error?.response?.status === 409) {
-        setError("Usuário já cadastrado, verifique os dados e tente novamente.");
+        setGeneralError("Usuário já cadastrado, verifique os dados e tente novamente.");
       } else {
-        setError("Erro ao cadastrar. Verifique os dados e tente novamente.");
+        setGeneralError("Erro ao cadastrar. Verifique os dados e tente novamente.");
       }
     } finally {
       setLoading(false);
     }
   };
 
-  const barraDeProgresso = `
-  .progress-custom {
-    height: 10px;
-    border: none;
-    background-color: #B0B8BE;
-    border-radius: 5px;
-  }
-  .progress-custom::-webkit-progress-bar {
-    background-color: #B0B8BE;
-    border-radius: 5px;
-  }
-  .progress-custom::-webkit-progress-value {
-    background-color: #1D98EA;
-    border-radius: 5px;
-  }
-`;
-
   return (
     <div
-      className="h-screen flex flex-col justify-center bg-cover bg-no-repeat bg-center"
+      className="cadastro-container"
       style={{ backgroundImage: "url(img/img-login.jpg)" }}
     >
-      <style jsx global>{`
-        ${barraDeProgresso}
-      `}</style>
-
-      <div className="w-[40rem] m-auto bg-white bg-opacity-80 p-5 rounded-lg shadow-lg text-gray-500">
-        <form>
+      <div className="cadastro-form-wrapper">
+        <form onSubmit={(e) => e.preventDefault()}>
           {etapa === 1 ? (
-            <div className="flex flex-col items-center gap-y-3">
-              <h2 className="text-2xl font-semibold mt-0 mb-4">Cadastro</h2>
+            <div className="cadastro-form-stage-1">
+              <h2 className="cadastro-form-title">Cadastro</h2>
               <progress
-                className={`progress-custom w-full mb-4`}
+                className="cadastro-progress"
                 max="100"
-                value={etapa === 1 ? 50 : 100}
+                value={50}
               />
-              <div className="flex w-full gap-x-2 border">
-                <div className="w-full">
-                  <p className="mb-2 text-left">Nome completo *</p>
+              <div className="cadastro-input-group">
+                <div className="cadastro-input-wrapper">
+                  <p className="cadastro-label">Nome de usuário *</p>
                   <input
                     type="text"
-                    placeholder=""
-                    className="w-full p-3 border-4 border-[#E5DCDC] rounded-lg"
-                    value={fullName}
-                    onChange={(e) => setFullName(e.target.value)}
-                  />
-                </div>
-                <div className="w-full">
-                  <p className="mb-2 text-left">Username *</p>
-                  <input
-                    type="text"
-                    placeholder=""
-                    className="w-full p-3 border-4 border-[#E5DCDC] rounded-lg"
+                    placeholder="Digite seu nome de usuário"
+                    className={`cadastro-input ${userNameError ? 'cadastro-input-error' : ''}`}
                     value={userName}
                     onChange={(e) => setUserName(e.target.value)}
                   />
+                  {userNameError && <p className="cadastro-error-message">{userNameError}</p>}
                 </div>
               </div>
-              <div className="w-full">
-                <p className="mb-2 text-left">Endereço de e-mail *</p>
+              <div className="cadastro-input-wrapper">
+                <p className="cadastro-label">Endereço de e-mail *</p>
                 <input
                   type="email"
                   placeholder="Digite seu e-mail"
-                  className="w-full p-3 border-4 border-[#E5DCDC] rounded-lg"
+                  className={`cadastro-input ${emailError ? 'cadastro-input-error' : ''}`}
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
-              </div>
-              <div className="flex w-full gap-x-2">
-                <div className="w-full">
-                  <p className="mb-2 text-left">Crie sua senha *</p>
-                  <input
-                    type="password"
-                    placeholder=""
-                    className="w-full p-3 border-4 border-[#E5DCDC] rounded-lg"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-                <div className="w-full">
-                  <p className="mb-2 text-left">Confirme sua senha *</p>
-                  <input
-                    type="password"
-                    placeholder=""
-                    className="w-full p-3 border-4 border-[#E5DCDC] rounded-lg"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                  />
-                </div>
-              </div>
-              <div className="w-full">
-                <p className="mb-2 text-left ">Selecione sua foto de perfil</p>
-                <input
-                  type="file"
-                  accept="image/png, image/jpeg"
-                  className="w-full cursor-pointer rounded-lg border outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-gray-500 file:bg-whiter file:px-5 file:py-3 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-form-strokedark dark:file:text-gray-700  dark:focus:border-primary"
-                  onChange={(e) => {
-                    const selectedFile = e.target.files?.[0];
-                    if (selectedFile) setFile(selectedFile);
-                  }}
-                />
+                {emailError && <p className="cadastro-error-message">{emailError}</p>}
               </div>
 
-              {error && (
-                <div className="border-red-600 rounded text-red-600 my-2 px-2 border">
-                  {error}
+              <div className="cadastro-input-wrapper">
+                <p className="cadastro-label">Crie sua senha *</p>
+                <input
+                  type="password"
+                  placeholder="Digite a senha"
+                  className={`cadastro-input ${passwordError ? 'cadastro-input-error' : ''}`}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {passwordError && <p className="cadastro-error-message">{passwordError}</p>}
+              </div>
+
+              <div className="cadastro-input-wrapper">
+                <p className="cadastro-label">Confirme sua senha *</p>
+                <input
+                  type="password"
+                  placeholder="Digite a senha novamente"
+                  className={`cadastro-input ${confirmPasswordError ? 'cadastro-input-error' : ''}`}
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                {confirmPasswordError && <p className="cadastro-error-message">{confirmPasswordError}</p>}
+              </div>
+
+              {generalError && (
+                <div className="cadastro-error">
+                  {generalError}
                 </div>
               )}
-              <div className="w-full px-3 flex justify-between items-center">
-                <Link href="/" className="px-3 text-gray-500">
+              <div className="btn-container">
+                <Link href="/" className="cadastro-back-link">
                   Voltar
                 </Link>
                 <button
                   type="button"
-                  className="bg-blue-500 text-white py-3 px-10 rounded-lg hover:bg-blue-600"
+                  className="cadastro-next-button"
+                  onClick={proximaEtapa}
+                >
+                  Próximo
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className="cadastro-form-stage-2">
+              <h2 className="cadastro-form-title">Cadastro</h2>
+              <progress
+                className="cadastro-progress"
+                max="100"
+                value={100}
+              />
+
+              <div className="cadastro-input-wrapper">
+                <p className="cadastro-label">Nome de exibição *</p>
+                <input
+                  type="text"
+                  placeholder="Digite o nome a ser apresentado"
+                  className={`cadastro-input ${fullNameError ? 'cadastro-input-error' : ''}`}
+                  value={fullName}
+                  onChange={(e) => setFullName(e.target.value)}
+                />
+                {fullNameError && <p className="cadastro-error-message">{fullNameError}</p>}
+              </div>
+
+              <div className="mb-6">
+                <div className="profile-picture-container">
+                  <img
+                    className="profile-picture"
+                    src={profilePicturePreview || '/img/user-null.png'}
+                    alt="Foto de Perfil"
+                  />
+                  <div>
+                    <label htmlFor="profilePictureInput" className="cadastro-label">
+                      Selecionar nova foto
+                    </label>
+                    <div className="text-sm text-gray-600">Extensões suportadas: PNG, JPEG</div>
+                    <div className="text-sm text-gray-600">Tamanho máximo: 5MB</div>
+                    <input
+                      type="file"
+                      id="profilePictureInput"
+                      className={`cadastro-file-input shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline ${fileError ? 'cadastro-input-error' : ''}`}
+                      onChange={handleFileChange}
+                      accept="image/png, image/jpeg"
+                    />
+                    {fileError && <p className="cadastro-error-message">{fileError}</p>}
+                  </div>
+                </div>
+              </div>
+
+              {generalError && (
+                <div className="cadastro-error">
+                  {generalError}
+                </div>
+              )}
+              <div className="cadastro-buttons-stage-2">
+                <button
+                  type="button"
+                  className="cadastro-submit-button"
                   disabled={loading}
                   onClick={register}
                 >
@@ -221,45 +305,14 @@ const Cadastro = () => {
                     "Cadastrar"
                   )}
                 </button>
-              </div>
-            </div>
-          ) : (
-            <div className="flex flex-col items-center">
-              {/* <h2 className="text-2xl font-semibold mt-0 mb-4">Cadastro</h2>
-              <progress
-                className={`progress-custom w-4/5 mb-4`}
-                max="100"
-                value={100}
-              />
-              <p className="w-4/5 mb-2 text-left">Nome de exibição *</p>
-              <input
-                type="text"
-                placeholder="Digite o nome a ser apresentado"
-                className="w-4/5 p-3 border-4 border-[#E5DCDC] rounded-lg"
-              />
-              <p className="w-4/5 mb-2 text-left">
-                Selecione sua foto de perfil
-              </p>
-              <input
-                type="file"
-                accept="image/png, image/jpeg"
-                className="w-4/5 mb-4"
-              />
-              <div className="flex flex-col items-center w-4/5 gap-4">
                 <button
-                  type="submit"
-                  className="bg-blue-500 text-white py-3 px-6 rounded-lg hover:bg-blue-600 w-full"
-                >
-                  CADASTRAR
-                </button>
-                <a
-                  href="#"
+                  type="button"
                   onClick={etapaAnterior}
-                  className="text-gray-500 bg-gray-200 py-3 px-6 rounded-lg w-full text-center"
+                  className="cadastro-back-button-stage-2"
                 >
                   Voltar
-                </a>
-              </div> */}
+                </button>
+              </div>
             </div>
           )}
         </form>
